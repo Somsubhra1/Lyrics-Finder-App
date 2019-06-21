75d6254b6f2286e3da379cf95a91a36e147da3ef
Somsubhra Das <das.somsubhra1@gmail.com>
2019-06-21 15:16:57 +0530

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
     return (
         <div className="App">
-            <h1>Hello</h1>
+            <Navbar />
         </div>
     );
 }
