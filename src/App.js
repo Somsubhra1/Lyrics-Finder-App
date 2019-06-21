32879205a7dfe3c0f47970c2fc0079366e133844
Somsubhra Das <das.somsubhra1@gmail.com>
2019-06-21 23:06:15 +0530

    Created lyrics component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
 import { Provider } from "./context";
+import Lyrics from "./components/tracks/Lyrics";
 
 function App() {
     return (
@@ -14,6 +15,11 @@ function App() {
                     <div className="container">
                         <Switch>
                             <Route exact path="/" component={Index} />
+                            <Route
+                                exact
+                                path="/lyrics/track/:id"
+                                component={Lyrics}
+                            />
                         </Switch>
                     </div>
                 </React.Fragment>
