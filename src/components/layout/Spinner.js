9eb12d92e3ff2b0a64a80663c3a2752d8a16789b
Somsubhra Das <das.somsubhra1@gmail.com>
2019-06-21 16:16:46 +0530

    Spinner created

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import spinner from './spinner.gif'
 
 const Spinner = () => {
     return (
         <div>
-            <img src={spinner} alt="Loading..." style={{width: "200px", margin: "40px auto", display: "block" }} />
+            <img
+                src={spinner}
+                alt="Loading..."
+                style={{
+                    width: "200px",
+                    margin: "40px auto",
+                    display: "block"
+                }}
+            />
         </div>
-    )
-}
+    );
+};
 
-export default Spinner
+export default Spinner;
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
+import Spinner from "../layout/Spinner";
 
 export default class Tracks extends Component {
     render() {
         return (
             // Consumer is the wrapper
             <Consumer>
-                {value => { // value is the values passed to Context.Provider
+                {value => {
+                    // value is the values passed to Context.Provider
                     console.log(value);
-                    return <h1>Tracks</h1>;
+                    const { track_list } = value;
+                    if (track_list === "undefinded" || track_list.length === 0) {
+                        return <Spinner />
+                    }
+                    return <h1>Tracks loaded</h1>;
                 }}
             </Consumer>
         );
