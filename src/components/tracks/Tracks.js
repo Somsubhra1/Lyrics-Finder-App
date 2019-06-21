9385b9aed9e0110bbd987c19a7204a9d6fd725c5
Somsubhra Das <das.somsubhra1@gmail.com>
2019-06-21 16:56:45 +0530

    Track component created

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
 import Spinner from "../layout/Spinner";
+import Track from "./Track";
 
 export default class Tracks extends Component {
     render() {
@@ -9,12 +10,27 @@ export default class Tracks extends Component {
             <Consumer>
                 {value => {
                     // value is the values passed to Context.Provider
-                    console.log(value);
-                    const { track_list } = value;
-                    if (track_list === "undefinded" || track_list.length === 0) {
-                        return <Spinner />
+                    // console.log(value);
+                    const { track_list, heading } = value;
+                    if (
+                        track_list === "undefinded" ||
+                        track_list.length === 0
+                    ) {
+                        return <Spinner />;
                     }
-                    return <h1>Tracks loaded</h1>;
+                    return (
+                        <React.Fragment>
+                            <h3 className="text-center mb-4">{heading}</h3>
+                            <div className="row">
+                                {track_list.map(item => (
+                                    <Track
+                                        key={item.track.track_id}
+                                        track={item.track}
+                                    />
+                                ))}
+                            </div>
+                        </React.Fragment>
+                    );
                 }}
             </Consumer>
         );
