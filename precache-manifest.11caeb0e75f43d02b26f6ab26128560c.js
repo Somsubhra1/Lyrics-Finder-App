559fc03c73fc7b12e6b5360d0a45a00b3a1608bd
Somsubhra Das <das.somsubhra1@gmail.com>
2019-06-22 17:05:45 +0530

    Updates

---
+++ precache-manifest.11caeb0e75f43d02b26f6ab26128560c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63b40177146a8db85565f237c6f83fa4",
+    "url": "/Lyrics-Finder-App/index.html"
+  },
+  {
+    "revision": "fbecdb98ef779911fed7",
+    "url": "/Lyrics-Finder-App/static/css/main.1fa3f329.chunk.css"
+  },
+  {
+    "revision": "77ba5f71544b73b328e2",
+    "url": "/Lyrics-Finder-App/static/js/2.bd45e88c.chunk.js"
+  },
+  {
+    "revision": "fbecdb98ef779911fed7",
+    "url": "/Lyrics-Finder-App/static/js/main.bcdbab7a.chunk.js"
+  },
+  {
+    "revision": "aaa8bf10eca53ea9e9e7",
+    "url": "/Lyrics-Finder-App/static/js/runtime~main.453e19ca.js"
+  }
+]);
