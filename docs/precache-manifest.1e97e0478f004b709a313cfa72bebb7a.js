6801dcf2d2deaa87ec7c4007a159b3924d73ea16
Somsubhra Das <das.somsubhra1@gmail.com>
2019-06-22 16:57:13 +0530

    Added docs directory

---
+++ docs/precache-manifest.1e97e0478f004b709a313cfa72bebb7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97882c52ae3508b92cc232b156bcd119",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbecdb98ef779911fed7",
+    "url": "/static/css/main.1fa3f329.chunk.css"
+  },
+  {
+    "revision": "77ba5f71544b73b328e2",
+    "url": "/static/js/2.bd45e88c.chunk.js"
+  },
+  {
+    "revision": "fbecdb98ef779911fed7",
+    "url": "/static/js/main.bcdbab7a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
